b2add445d89ba05c0f1253c5e9e6fcb8c9980324
almeidatercero <almeida.tercero@gmail.com>
2016-08-17 10:53:53 -0500

    nice

---
+++ js/config.js
@@ -0,0 +1,83 @@
+$(function() {
+
+  CMS.init({
+
+    // Name of your site or location of logo file, relative to root directory (img/logo.png)
+    siteName: 'ma blog',
+
+    // Tagline for your site
+    siteTagline: 'I <3 Design',
+
+    // Email address
+    siteEmail: 'almeida.tercero@gmail.com.com',
+
+    // Name
+    siteAuthor: 'almeidatercero',
+
+    // Navigation items
+    siteNavItems: [
+      { name: 'Github', href: 'https://github.com//almeidatercero', newWindow: false},
+      { name: 'About'},
+      { name: 'Contact'}
+
+    ],
+
+    // Posts folder name
+    postsFolder: 'posts',
+
+    // Homepage posts snippet length
+    postSnippetLength: 120,
+
+    // Pages folder name
+    pagesFolder: 'pages',
+
+    // Order of sorting (true for newest to oldest)
+    sortDateOrder: true,
+
+    // Posts on Frontpage (blog style)
+    postsOnFrontpage: true,
+
+    // Page as Frontpage (static)
+    pageAsFrontpage: '',
+
+    // Posts/Blog on different URL
+    postsOnUrl: '',
+
+    // Site fade speed
+    fadeSpeed: 300,
+
+    // Site footer text
+    footerText: '&copy; ' + new Date().getFullYear() + ' All Rights Reserved.',
+
+    // Mode 'Github' for Github Pages, 'Server' for Self Hosted. Defaults
+    // to Github
+    mode: 'Github',
+
+     // If Github mode is set, your Github username and repo name.
+    githubUserSettings: {
+      username: 'yourusername',
+      repo: 'yourrepo'
+    },
+
+    // If Github mode is set, choose which Github branch to get files from.
+    // Defaults to Github pages branch (gh-pages)
+    githubSettings: {
+      branch: 'gh-pages',
+      host: 'https://api.github.com'
+    }
+
+  });
+
+  // Markdown settings
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false
+  });
+
+});
---
+++ pages/contact.md
@@ -0,0 +1,12 @@
+---
+layout: default
+title: Contact
+---
+
+Esto es mi contacto!
+
+* [Smashing Magazine](https://www.smashingmagazine.com)
+* [TechCrunch](http://techcrunch.com/)
+* [Ars Technica](http://arstechnica.com/)
+* [Codoki](http://codoki.com/)
+* [Mozilla Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
